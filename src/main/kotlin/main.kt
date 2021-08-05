255aa74e6e98709a32ea497e781c4a30613a4bab
GuiMonLopes <guilherme.monteiro1@gmail.com>
2021-08-04 22:32:32 -0300

    Introdução ao Kotlin

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,11 @@
 fun main(){
-    print("Bem Vindo ao ByteBank!")
-    val titular = "Alex"
+    println("Bem Vindo ao ByteBank!")
+    var titular = "Alex"
+    val numeroConta = 1000
+    var saldo = 0.00
+
+    println("Titular: $titular")
+    println("Numero da Conta: $numeroConta")
+    println("Saldo da conta: $saldo")
 
 }
