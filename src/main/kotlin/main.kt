7e2391c8b91132780322d53215c2f0d25ce15c50
OrangeBox <orangebox@Diegos-Mac-mini.local>
2021-08-09 12:05:31 -0300

    Criacao de Classes no Kotlin

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,52 @@
 fun main() {
-
-
     println("Bem Vindo ao ByteBank!")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.00
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
 
-    for (i in 1..5) {
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println("Depositando na conta ")
+    deposita(contaAlex,50.0)
+    println(contaAlex.saldo)
+    deposita(contaFran, 70.0)
+
+
+    }
+
+class Conta{
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+fun deposita(conta: Conta, valor: Double){
+    conta.saldo += valor
+}
+
+
+
+fun testaLacos(){
+    //    for (i in 5 downTo 1 step 2) {
+//        var titular = "Alex $i"
+//        val numeroConta = 1000 + i
+//        var saldo = i + 10.0
+//
+//        println("Titular: $titular")
+//        println("Numero da Conta: $numeroConta")
+//        println("Saldo da conta: $saldo")
+
+    var i = 0
+    while (i<=5){
         var titular = "Alex $i"
         val numeroConta = 1000 + i
         var saldo = i + 10.0
@@ -11,12 +54,12 @@ fun main() {
         println("Titular: $titular")
         println("Numero da Conta: $numeroConta")
         println("Saldo da conta: $saldo")
-
-//    testaCondicoes(saldo)
-
+        i++
     }
 
 }
+
+
 
 fun testaCondicoes(saldo: Double) {
 
@@ -43,3 +86,4 @@ fun testaCondicoes(saldo: Double) {
     }
 
 }
+
