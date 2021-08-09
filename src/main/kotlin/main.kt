ee80c76025536c7b2c010d346cf5e07167ebd995
Diego Mondego <diego@orangebox.technology>
2021-08-09 09:15:16 -0300

    Loop

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,45 @@
-fun main(){
-    println("Bem Vindo ao ByteBank!")
-    var titular = "Alex"
-    val numeroConta = 1000
-    var saldo = 0.00
+fun main() {
 
-    println("Titular: $titular")
-    println("Numero da Conta: $numeroConta")
-    println("Saldo da conta: $saldo")
+
+    println("Bem Vindo ao ByteBank!")
+
+    for (i in 1..5) {
+        var titular = "Alex $i"
+        val numeroConta = 1000 + i
+        var saldo = i + 10.0
+
+        println("Titular: $titular")
+        println("Numero da Conta: $numeroConta")
+        println("Saldo da conta: $saldo")
+
+//    testaCondicoes(saldo)
+
+    }
+
+}
+
+fun testaCondicoes(saldo: Double) {
+
+    //    if (saldo > 0.0) {
+//        println("Conta é posistiva")
+//    }
+//    else if (saldo == 0.0) {
+//        println("Conta é neutra")
+//    }
+//    else {
+//        println("Conta negativa")
+//    }
+
+    when {
+        saldo > 0.0 -> {
+            println("Conta é posistiva")
+        }
+        saldo == 0.0 -> {
+            println("Conta é neutra")
+        }
+        else -> {
+            println("Conta negativa")
+        }
+    }
 
 }
